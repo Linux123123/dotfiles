6916e67929a9cc60fd59141ed10ffc2ec905a8b0
Linux123123 <linas.alexx@gmail.com>
2020-12-21 17:59:46 +0200

    Add bin

---
+++ .local/bin/multiple-monitors.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output HDMI-A-0 --mode 1920x1080 --pos 1920x0 --rotate normal --output HDMI-A-3 --mode 1920x1080 --pos 1920x0 --rotate normal --output DP1 --mode 1920x1080 --pos 0x98 --rotate normal
