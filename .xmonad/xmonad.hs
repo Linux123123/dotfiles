9b0a1aca991e7719b584c605fd6f44aa1cadd9e2
Linux123123 <linas.alexx@gmail.com>
2021-01-04 19:00:24 +0200

    Update xmonad.hs

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -151,7 +151,7 @@ myKeys =
         , ("M-S-p", spawn myEditor)
         , ("M-S-b", spawn myBrowser)
         , ("M-S-d", spawn "discord")
-        , ("M-S-m", spawn "$HOME/Applications/youtube-music.sh")
+        , ("M-S-m", spawn "exec $HOME/Applications/YouTube-Music-1.8.0.AppImage")
 
     -- Language
         , ("M-S-o", spawn "$HOME/.local/bin/layout_switch")
