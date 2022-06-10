2bd26aaab151924589104be59a6cc38f974aed5e
Linux123123 <me@linux123123.com>
2022-06-10 18:34:49 +0300

    Update

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,4 +1,4 @@
-set -U fish_user_paths $fish_user_paths $HOME/.local/bin/ $HOME/.config/composer/vendor/bin (yarn global bin) $ANDROID_HOME/emulator $ANDROID_HOME/tools $ANDROID_HOME/tools/bin $ANDROID_HOME/platform-tools
+set -U fish_user_paths $fish_user_paths $HOME/.local/bin/ (yarn global bin)
 set fish_greeting                      # Supresses fish's intro message
 set TERM "xterm-256color"              # Sets the terminal type
 set EDITOR "code"                      # $EDITOR
@@ -13,11 +13,6 @@ set fish_color_autosuggestion '#7d7d7d'
 set fish_color_command brcyan
 set fish_color_error '#ff6c6b'
 set fish_color_param brcyan
-
-# Function for creating a backup file
-function backup --argument filename
-    cp $filename $filename.bak
-end
 
 ### ALIASES ###
 # doas as sudo
@@ -37,21 +32,9 @@ alias la='exa -a --color=always --group-directories-first'  # all files and dirs
 alias ll='exa -l --color=always --group-directories-first'  # long format
 alias lt='exa -aT --color=always --group-directories-first' # tree listing
 
-# Change cat to bat
-alias cat='bat'
-
-# pacman and yay
-alias pacsyu='sudo pacman -Syyu'                 # update only standard pkgs
-alias yaysua='yay -Sua --noconfirm'              # update only AUR pkgs
-alias yaysyu='yay -Syu --noconfirm'              # update standard pkgs and AUR pkgs
-alias unlock='sudo rm /var/lib/pacman/db.lck'    # remove pacman lock
-alias cleanup='sudo pacman -Rns (pacman -Qtdq)'  # remove orphaned packages
-
-# get fastest mirrors
-alias mirror="sudo reflector -f 30 -l 30 --number 10 --verbose --save /etc/pacman.d/mirrorlist"
-
 # Colorize grep output (good for log files)
 alias grep='grep --color=auto'
+alias beak='/home/linux123123/Github/ptero-dev-v1/beak'
 
 # confirm before overwriting something
 alias cp="cp -i"
@@ -62,12 +45,6 @@ alias rm='rm -i'
 alias df='df -h'                          # human-readable sizes
 alias free='free -m'                      # show sizes in MB
 
-## get top process eating memory
-alias psmem3='ps auxf | sort -nr -k 4 | head -3'
-
-## get top process eating cpu
-alias pscpu3='ps auxf | sort -nr -k 3 | head -3'
-
 # the terminal rickroll
 alias rr='curl -s -L https://raw.githubusercontent.com/keroserene/rickrollrc/master/roll.sh | bash'
 
@@ -75,3 +52,6 @@ alias rr='curl -s -L https://raw.githubusercontent.com/keroserene/rickrollrc/mas
 alias config="/usr/bin/git --git-dir=$HOME/dotfiles --work-tree=$HOME"
 
 starship init fish | source
+
+# Brew
+eval (/home/linuxbrew/.linuxbrew/bin/brew shellenv)
